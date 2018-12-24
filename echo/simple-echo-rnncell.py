22584315eadbdf0c72148a61e689d4d57f9c3425
steven <stevenliucx@gmail.com>
2018-12-24 19:15:00 +0800

    simple echo

--- echo/simple-echo-rnncell.py
+++ echo/simple-echo-rnncell.py
@@ -1,5 +1,6 @@
 """
 A simple program to implement a rnn network to echo input character.
+Using rnn cells.
 """
 
 from __future__ import print_function, division
@@ -33,26 +34,32 @@ batchY_placeholder = tf.placeholder(tf.int32, [batch_size, truncated_backprop_le
 
 init_state = tf.placeholder(tf.float32, [batch_size, state_size])
 
-W = tf.Variable(np.random.rand(state_size+1, state_size), dtype=tf.float32)
-b = tf.Variable(np.zeros((1,state_size)), dtype=tf.float32)
+#W = tf.Variable(np.random.rand(state_size+1, state_size), dtype=tf.float32)
+#b = tf.Variable(np.zeros((1,state_size)), dtype=tf.float32)
 
 W2 = tf.Variable(np.random.rand(state_size, num_classes),dtype=tf.float32)
 b2 = tf.Variable(np.zeros((1,num_classes)), dtype=tf.float32)
 
 # Unpack columns
-inputs_series = tf.unstack(batchX_placeholder, axis=1)
+# inputs_series = tf.unstack(batchX_placeholder, axis=1)
+inputs_series = tf.split(batchX_placeholder, truncated_backprop_length, axis=1)
 labels_series = tf.unstack(batchY_placeholder, axis=1)
+#print("inputs: %s, labels: %s" % (inputs_series.get_shape(), labels_series.get_shape()))
 
+cell = tf.nn.rnn_cell.BasicRNNCell(state_size)
+#states_series, current_state = tf.nn.rnn(cell, inputs_series, init_state)
+states_series, current_state = tf.nn.static_rnn(cell, inputs_series, initial_state=init_state,dtype=tf.float32)
+#
 # Forward pass
-current_state = init_state
-states_series = []
-for current_input in inputs_series:
-    current_input = tf.reshape(current_input, [batch_size, 1])
-    input_and_state_concatenated = tf.concat([current_input, current_state], axis=1)  # Increasing number of columns
-
-    next_state = tf.tanh(tf.matmul(input_and_state_concatenated, W) + b)  # Broadcasted addition
-    states_series.append(next_state)
-    current_state = next_state
+#current_state = init_state
+#states_series = []
+#for current_input in inputs_series:
+#    current_input = tf.reshape(current_input, [batch_size, 1])
+#    input_and_state_concatenated = tf.concat([current_input, current_state], axis=1)  # Increasing number of columns
+#
+#    next_state = tf.tanh(tf.matmul(input_and_state_concatenated, W) + b)  # Broadcasted addition
+#    states_series.append(next_state)
+#    current_state = next_state
 
 
 logits_series = [tf.matmul(state, W2) + b2 for state in states_series] #Broadcasted addition
